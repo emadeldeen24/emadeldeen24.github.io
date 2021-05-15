879db7e2ebd910a282e56985761128556c7f5a8c
Emadeldeen Eldele <37911596+emadeldeen24@users.noreply.github.com>
2021-05-15 08:27:44 +0800

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -5,3 +5,21 @@ AOS.init({
 });
 
 // Add your javascript here
+var acc = document.getElementsByClassName("accordion_mine");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
