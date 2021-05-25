e41543fceef49983300c78819cd171ef972e488b
Emadeldeen Eldele <37911596+emadeldeen24@users.noreply.github.com>
2021-05-25 20:35:58 +0800

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -3,35 +3,3 @@ AOS.init({
   anchorPlacement: 'top-left',
   duration: 1000
 });
-
-// Add your javascript here
-var acc = document.getElementsByClassName("accordion_mine");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
-    this.classList.toggle("active");
-
-    /* Toggle between hiding and showing the active panel */
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
-}
-
-
-$(document).ready(function(){
-    $('[data-toggle="popover"]').popover();   
-});
-
-$('#attnsleepModal').on('shown.bs.modal', function () {
-  $('#myInput').trigger('focus')
-})
-$('#mirnaModal').on('shown.bs.modal', function () {
-  $('#myInput').trigger('focus')
-})
